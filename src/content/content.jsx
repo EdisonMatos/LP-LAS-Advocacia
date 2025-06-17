3f6937741bb4ffacbb00f09a14e68afaaf62b4d3
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-17 16:39:34 -0300

    fix: Subir comentario

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -8,6 +8,8 @@ import backgroundPersonMobile from "../assets/imgs/hero/bgHeroMobile.jpg";
 import imgFeatures from "../assets/imgs/features/imgServices.webp";
 import imgDivisor from "../assets/imgs/features/imgDivisor.jpg";
 
+// comentario
+
 import aboutImg1 from "../assets/imgs/about/aboutImg.webp";
 import imgAbout1 from "../assets/imgs/about/aboutImg1.jpg";
 import imgAbout2 from "../assets/imgs/about/aboutImg2.jpg";
