58915d6a21fe58b720a6e56fb0bac9c0b9af0ace
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-04 10:21:01 -0300

    fix: icon features

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -218,13 +218,12 @@ const content = {
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-stethoscope-icon lucide-stethoscope"
+            class="lucide lucide-briefcase-business-icon lucide-briefcase-business"
           >
-            <path d="M11 2v2" />
-            <path d="M5 2v2" />
-            <path d="M5 3H4a2 2 0 0 0-2 2v4a6 6 0 0 0 12 0V5a2 2 0 0 0-2-2h-1" />
-            <path d="M8 15a6 6 0 0 0 12 0v-3" />
-            <circle cx="20" cy="10" r="2" />
+            <path d="M12 12h.01" />
+            <path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" />
+            <path d="M22 13a18.15 18.15 0 0 1-20 0" />
+            <rect width="20" height="14" x="2" y="6" rx="2" />
           </svg>
         ),
         buttonLabelModal: "Fale Conosco",
